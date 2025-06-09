f2f31aac3fa2bc6cb537f6af8d4f625b3fdff941
sweta29singh <sweta.singh@cern.ch>
2025-06-09 13:54:17 +0530

    meanptfluct

--- PWGCF/EbyEFluctuations/Tasks/eventMeanPtId.cxx
+++ PWGCF/EbyEFluctuations/Tasks/eventMeanPtId.cxx
@@ -1,7 +1,6 @@
-// Copyright CERN and others contributing to the ALICE O2 project.
-// This file is part of the ALICE O2 software.
-// See https://alice-o2.web.cern.ch/copyright for details.
-// All rights reserved.
+// Copyright 2019-2020 CERN and copyright holders of ALICE O2.
+// See https://alice-o2.web.cern.ch/copyright for details of the copyright holders.
+// All rights not expressly granted are reserved.
 //
 // This software is distributed under the terms of the GNU General Public
 // License v3 (GPL Version 3), copied verbatim in the file "COPYING".
@@ -9,7 +8,7 @@
 // In applying this license CERN does not waive the privileges and immunities
 // granted to it by virtue of its status as an Intergovernmental Organization
 // or submit itself to any jurisdiction.
-//
+// 
 /// \file eventMeanPtId.cxx
 /// \brief Analysis task to study Mean pT Fluctuations using two particle correlator using Cumulant Method
 /// \author Sweta Singh (sweta.singh@cern.ch)
